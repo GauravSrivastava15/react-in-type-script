8ea3b2b907d3f173086939161fbfae6c99843960
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2024-06-23 18:55:04 +0530

    useState() hook implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import Box from "./components/Box"
+import { FormEvent, useState } from "react";
 
-
-function App() {
-  return (
-   <>
-   <div>
-      <Box  label="Search" value={""} onChange={() => {}}/>
-
-   </div>
-   </>
-  )
+interface Person {
+  name: string;
+  age: number;
 }
 
-export default App
+function App() {
+  const [user, setUser] = useState<Person>();
+  const submitHandler = (e:FormEvent<HTMLFormElement>) =>{
+    e.preventDefault()
+    console.log(user)
+  }
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          type="number"
+          placeholder="Age"
+          value={user?.age || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+          }
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          value={user?.name || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, name: e.target.value }))
+          }
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
